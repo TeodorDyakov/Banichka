2c97b9ed1b575b1b8b5b634f673df69d4fb4b488
TeodorDyakov <tedy1996@gmail.com>
2018-03-19 22:08:44 +0200

    add slr example

--- Banichka/src/regression/SimpleLinearRegression.java
+++ Banichka/src/regression/SimpleLinearRegression.java
@@ -7,8 +7,8 @@ public class SimpleLinearRegression {
 	private double alpha = 0, beta = 0;
 
 	public void fit(double[] x, double[] y) {
-		double sumX = Util.sum(x), sumY = Util.sum(y), sumXX = Util.sumOfSquares(x),
-				sumXY = Util.sumOfProducts(x, y);
+		double sumX = Util.sum(x), sumY = Util.sum(y), sumXX = Util.dotProduct(x, x),
+				sumXY = Util.dotProduct(x, y);
 		int n = x.length;
 
 		beta = (n * sumXY - sumX * sumY) / (n * sumXX - sumX * sumX);
@@ -24,19 +24,7 @@ public class SimpleLinearRegression {
 	}
 
 	public double predict(double x) {
-		return alpha * x + beta;
-	}
-
-	static double[] x = new double[] { 1.47, 1.50, 1.52, 1.55, 1.57, 1.60, 1.63, 1.65, 1.68, 1.70, 1.73, 1.75,
-			1.78, 1.80, 1.83 };
-	static double[] y = new double[] { 52.21, 53.12, 54.48, 55.84, 57.20, 58.57, 59.93, 61.29, 63.11, 64.47,
-			66.28, 68.10, 69.92, 72.19, 74.46 };
-
-	public static void main(String[] args) {
-		SimpleLinearRegression slr = new SimpleLinearRegression();
-		slr.fit(x, y);
-		System.out.println(slr.alpha);
-		System.out.println(slr.beta);
+		return alpha + x * beta;
 	}
 
 }
--- Banichka/src/util/Util.java
+++ Banichka/src/util/Util.java
@@ -43,15 +43,7 @@ public class Util {
 		return sum;
 	}
 
-	public static double sumOfSquares(double[] arr) {
-		double sum = 0.0;
-		for (double a : arr) {
-			sum += a * a;
-		}
-		return sum;
-	}
-
-	public static double sumOfProducts(double[] arr, double[] arr1) {
+	public static double dotProduct(double[] arr, double[] arr1) {
 		double sum = 0.0;
 		for (int i = 0; i < arr.length && i < arr1.length; i++) {
 			sum += arr[i] * arr1[i];
